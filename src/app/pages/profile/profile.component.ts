82d6ea642b5ce13ddc57aae6aa1468c8b864294a
Denis Paola <denislozanoc@gmail.com>
2023-08-23 10:15:51 +0200

    Initial commit

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+
+  user?: User;
+
+  constructor(){
+  }
+
+
+  modificar(nombre: HTMLInputElement, apellido: HTMLInputElement, correo:HTMLInputElement, url: HTMLInputElement){
+    debugger;
+    this.user = new User(0, nombre.value, apellido.value, correo.value, url.value,'');
+
+    console.log(this.user.name);
+  }
+
+}
