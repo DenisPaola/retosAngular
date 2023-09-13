5df2e9850477bcad00afdb8a52c5611cabb570c0
Denis Paola <denislozanoc@gmail.com>
2023-09-13 16:18:23 +0200

    correcion

--- src/app/pages/update-book/update-book.component.ts
+++ src/app/pages/update-book/update-book.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { BooksService } from '../../shared/books.service';
-<<<<<<< HEAD
-=======
 import {ToastrService} from "ngx-toastr";
->>>>>>> rama5
 
 @Component({
   selector: 'app-update-book',
@@ -15,11 +12,8 @@ export class UpdateBookComponent {
   bookUpdated: boolean = false;
   bookNoFinded: boolean = false;
 
-<<<<<<< HEAD
-  constructor(private booksService: BooksService) {}
-=======
+
   constructor(private booksService: BooksService, private toastr: ToastrService) {}
->>>>>>> rama5
 
   updateBook(idbook:HTMLInputElement, title: HTMLInputElement, type: HTMLInputElement, author: HTMLInputElement, price: HTMLInputElement, photo: HTMLInputElement): void {
     let book = this.booksService.getOne(parseInt(idbook.value))
@@ -29,16 +23,16 @@ export class UpdateBookComponent {
       book.author = author.value
       book.price = parseInt(price.value)
       book.photo = photo.value
-<<<<<<< HEAD
+
       this.booksService.edit(book);
       this.bookUpdated = true;
     }
     else{
       this.bookNoFinded = true
-=======
+
       this.bookUpdated = this.booksService.edit(book);
       if(this.bookUpdated){
-        this.toastr.success('Libro actualizado correctamente', 'Éxito');
+        this.toastr.success('Libro actualizado correctamente');
       }
       else{
         this.toastr.error('Libro no actualizado', '');
@@ -47,7 +41,7 @@ export class UpdateBookComponent {
     else{
       this.bookNoFinded = true
       this.toastr.error('Libro no encontrado', '');
->>>>>>> rama5
+
     }
   }
 }
